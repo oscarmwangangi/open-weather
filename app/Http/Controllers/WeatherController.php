b976de3199a1ee2e7a153d03e4f91d25bcdd70b0
oscar mwangangi <mwangangioscar2016@gmail.com>
2025-04-21 23:54:25 +0300

    first commit

---
+++ app/Http/Controllers/WeatherController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class WeatherController extends Controller
+{
+    public function getWeather(Request $request)
+    {
+        $city = $request->query('city', 'Nairobi');
+
+       
+        $apiKey = '9ae7ae5b44111ec0ef9513ab9ba2aebf';
+        
+        $response = Http::get("https://api.openweathermap.org/data/2.5/weather", [
+            'q' => $city,
+            'appid' => $apiKey,
+            'units' => 'metric'
+        ]);
+
+        return $response->json();
+    }
+}
